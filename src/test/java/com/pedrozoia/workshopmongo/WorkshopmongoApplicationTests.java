baacffcab37c031e6451a4bbe443fb1eab9edbfb
Pedro Zoia <pedrocauzivert@gmail.com>
2025-12-18 11:06:30 -0300

    Project created

---
+++ src/test/java/com/pedrozoia/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrozoia.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
